02a2b147f9b7a8a5b6c4b0418525ee1554b86b12
Lee <batespcm1981@gmail.com>
2020-07-21 13:03:36 +0100

    util testing for within50 filter

---
+++ utils/utils.js
@@ -0,0 +1,18 @@
+// Latitude of 51.509865 & Longitude 0.118092 taken from latlong.net
+
+const latNorth = 52.2332;
+const latSouth = 50.78653;
+const longWest = -1.2802;
+const longEast = 1.044016;
+
+exports.withinFifty = users => {
+  const filteredUsers = users.filter(user => {
+    return (
+      parseFloat(user.latitude) >= latSouth &&
+      parseFloat(user.latitude) <= latNorth &&
+      parseFloat(user.longitude) >= longWest &&
+      parseFloat(user.longitude) <= longEast
+    );
+  });
+  return filteredUsers;
+};
