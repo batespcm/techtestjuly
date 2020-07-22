5aa28714e4a402fe7e48cd3b814dd3f77c16d34f
Lee <batespcm1981@gmail.com>
2020-07-22 22:17:25 +0100

    error controllers removed from app

--- app.js
+++ app.js
@@ -2,14 +2,11 @@ const express = require('express');
 const app = express();
 const cors = require('cors');
 const apiRouter = require('./routes/apiRouter.js');
-const { handleInternalErrors } = require('./controllers/error.controllers');
 
 app.use(cors());
 
 app.use(express.json());
 
 app.use('/api', apiRouter);
-
-app.use(handleInternalErrors);
 
 module.exports = app;
