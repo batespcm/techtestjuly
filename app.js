51e2a5dd2029b291c875e7880e80b07a1ca16a6b
Lee <batespcm1981@gmail.com>
2020-07-22 20:12:25 +0100

    handle internal error created in app

--- app.js
+++ app.js
@@ -2,11 +2,14 @@ const express = require('express');
 const app = express();
 const cors = require('cors');
 const apiRouter = require('./routes/apiRouter.js');
+const { handleInternalErrors } = require('./controllers/error.controller');
 
 app.use(cors());
 
 app.use(express.json());
 
 app.use('/api', apiRouter);
+
+app.use(handleInternalErrors);
 
 module.exports = app;
