4db8e46baca600642c0e8174f2ecffb24dd57d61
Lee <batespcm1981@gmail.com>
2020-07-22 13:56:58 +0100

    file cleanup of old code before endpoint changes

--- utils/apiCalls.js
+++ utils/apiCalls.js
@@ -12,33 +12,7 @@ exports.londonListed = () => {
     });
 };
 
-// exports.allUsersWithinFifty = () => {
-//   // Latitude of 51.509865 & Longitude 0.118092 taken from latlong.net
-//   const latNorth = 52.2332;
-//   const latSouth = 50.78653;
-//   const longWest = -1.2802;
-//   const longEast = 1.044016;
-
-//   return axios.get('https://bpdts-test-app.herokuapp.com/users').then(res => {
-//     const withinFifty = res.data.filter(user => {
-//       return (
-//         parseFloat(user.latitude) >= latSouth &&
-//         parseFloat(user.latitude) <= latNorth &&
-//         parseFloat(user.longitude) >= longWest &&
-//         parseFloat(user.longitude) <= longEast
-//       );
-//     });
-//     return withinFifty;
-//   });
-// };
-
 exports.allUsersWithinFifty = () => {
-  // Latitude of 51.509865 & Longitude 0.118092 taken from latlong.net
-  const latNorth = 52.2332;
-  const latSouth = 50.78653;
-  const longWest = -1.2802;
-  const longEast = 1.044016;
-
   return axios.get('https://bpdts-test-app.herokuapp.com/users').then(res => {
     return isWithinRadius(res.data);
   });
